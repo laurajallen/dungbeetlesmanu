709c20a5f8e9b5f7e05355f10bea52f81a6205ac
laurajallen <laurajallen@hotmail.com>
2019-04-28 19:12:42 +0100

    added overdispersion test function

--- correlation_morans_functions.r
+++ correlation_morans_functions.r
@@ -25,3 +25,18 @@ docor <- function(ia,ib){
   return(cor)
 }
 
+### test for overdispersion http://bbolker.github.io/mixedmodels-misc/glmmFAQ.html#testing-for-overdispersioncomputing-overdispersion-factor
+overdisp_fun <- function(model) {
+  ## number of variance parameters in 
+  ##   an n-by-n variance-covariance matrix
+  vpars <- function(m) {
+    nrow(m)*(nrow(m)+1)/2
+  }
+  model.df <- sum(sapply(VarCorr(model),vpars))+length(fixef(model))
+  rdf <- nrow(model.frame(model))-model.df
+  rp <- residuals(model,type="pearson")
+  Pearson.chisq <- sum(rp^2)
+  prat <- Pearson.chisq/rdf
+  pval <- pchisq(Pearson.chisq, df=rdf, lower.tail=FALSE)
+  c(chisq=Pearson.chisq,ratio=prat,rdf=rdf,p=pval)
+}
